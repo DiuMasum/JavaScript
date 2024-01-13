dbcf1e1086f1b56c34203c85430f05fe506eadef
DiuMasum <masum35-577@diu.edu.bd>
2024-01-13 23:44:35 +0600

    operators

---
+++ operators.js
@@ -0,0 +1,39 @@
+// arithmetic operators = operands (values, variables, etc.)
+//                        operators (+ - * /)
+//                        ex. 11 = x + 5;
+
+
+let student = 30;
+
+//student = student +1;
+//student = student -1;
+//student = student * 2;
+//student = student / 2;
+//student = student ** 2;
+//student = student % 2;
+
+//student += 1;
+//student -= 1;
+//student *=2;
+//student /= 2;
+//student **= 2;
+//student %=2;
+
+//student++;
+//student--; 
+
+console.log(student);
+
+
+
+/*
+    operator precedence
+    1. paranthesis ()
+    2. exponents
+    3. multiplication & division & modulo
+    4. addition & subtraction
+*/
+
+let result = 1+2*3+4**2;
+
+console.log(result);
