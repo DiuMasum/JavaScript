511d852f6a500b452ae3e39f88541d4425985f33
DiuMasum <masum35-577@diu.edu.bd>
2024-01-13 23:04:42 +0600

    variable

---
+++ variable.js
@@ -0,0 +1,24 @@
+// variable = A container that stores a value. Behaves as if it were the value it contains.
+
+let x;
+x=10;
+
+console.log(x);
+
+
+let age = 23;
+let price = 11.90;
+let cgpa = 3.50;
+
+console.log(typeof price)
+console.log(age);
+console.log(`your age is ${age}`);
+console.log(`the price is $${price}`);
+console.log(`and your cgpa is ${cgpa}`);
+
+
+let online = true;
+let forsale = false;
+
+console.log(`bro is online: ${online}`);
+console.log(`this is for sale: ${forsale}`);
