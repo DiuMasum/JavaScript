1488d2f651c6cf61281491b330fa9925dd862cf9
DiuMasum <masum35-577@diu.edu.bd>
2024-01-14 11:15:42 +0600

    type conversion

---
+++ TypeConversion.js
@@ -0,0 +1,14 @@
+// type conversion = change the datatypes of a value to another
+//                   (strings, numbers, booleans)
+
+let x = "singara";
+let y = "singara";
+let z = "singara";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
