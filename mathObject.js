c40080e5509f84bf39288241e25b1dd2237dc8e2
DiuMasum <masum35-577@diu.edu.bd>
2024-01-15 00:34:49 +0600

    math objects

---
+++ mathObject.js
@@ -0,0 +1,21 @@
+// Math = built in object that rovides a collection of properties and methods
+
+console.log(Math.PI);
+
+
+let x = 3;
+let y = 2;
+let z = 1;
+
+//z = Math.round(x);
+//z = Math.floor(x);
+//z = Math.ceil(x);
+//z = Math.trunc(x);
+//z = Math.pow(x, y);
+//z = Math.log(x);
+//z = Math.abs(x);
+let max = Math.max(x, y, z);
+let min = Math.min(x, y, z);
+
+console.log(max);
+console.log(min);
