71377425f1cca22bca3493c1ec2eb8deb28c797e
DiuMasum <masum35-577@diu.edu.bd>
2024-01-16 02:46:44 +0600

    for loop

---
+++ forLoops.js
@@ -0,0 +1,19 @@
+// for loop = repeat some code a LIMITED amount of times
+
+
+//example 1
+for (let i=1; i<=10; i++){
+    console.log(`bangladesh`);
+}
+
+
+
+//example 2
+for(let n=1; n<=15; n++){
+    if(n==12){
+        break;
+    }
+    else{
+        console.log(n);
+    }
+}
