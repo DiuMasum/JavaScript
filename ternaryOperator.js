3b9c84a512bc7886b03e4f415f4fef2f401875c8
DiuMasum <masum35-577@diu.edu.bd>
2024-01-15 12:28:38 +0600

    ternary operator

---
+++ ternaryOperator.js
@@ -0,0 +1,16 @@
+// ternary operator = a shortcut to if{} and else{} statements
+//                    helps to assign a variable base on a condition
+//                    condition ? codeIfTrue : codeIfFalse
+
+let age = 19;
+let message = age>=18 ? "you are an adult" : "your are minor";
+console.log(message);
+
+let time = 9;
+let greeting = time < 12 ? "good morning" : "good afternoon";
+console.log(greeting);
+
+let isStudent = true;
+let said = isStudent ? "youre a student" : "your are not a student";
+console.log(said);
+
