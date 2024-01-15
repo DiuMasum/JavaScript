366b595b2b29ea3ecf816c410f276868905bee93
DiuMasum <masum35-577@diu.edu.bd>
2024-01-15 13:02:44 +0600

    switch case statement

---
+++ switch.js
@@ -0,0 +1,45 @@
+let day = 8;
+
+switch(day){
+    case 1:
+    console.log("saturday");
+    break;
+    case 2:
+    console.log("sunday");
+    break;
+    case 3:
+    console.log("monday");
+    break;
+    case 4:
+    console.log("tuesday");
+    break;
+    case 5:
+    console.log("wednessday");
+    break;
+    case 6:
+    console.log("tursday");
+    break;
+    case 7:
+    console.log("friday");
+    break;
+    
+    default:
+    console.log("this is not a day");
+}
+
+
+let marks = 80;
+let grade;
+
+switch(true){
+    case marks >=80:
+        console.log("A");
+        break;
+    case marks >=70:
+        console.log("B");
+        break;
+    case marks >=60:
+        console.log("C");
+        break;
+}
+
