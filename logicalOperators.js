74034bd53ac449b6f0a36317eb78b358cbf00412
DiuMasum <masum35-577@diu.edu.bd>
2024-01-16 01:25:06 +0600

    logical operators

---
+++ logicalOperators.js
@@ -0,0 +1,15 @@
+// logical operators = used to combine or manipulate boolean values
+//                     (true of false)
+//           
+//                    AND = &&
+//                    OR = ||
+//                    NOT = !
+
+const temp = 100;
+
+if(temp>0 && temp<=30){
+    console.log("weather is good");
+}
+else{
+    console.log("weather is bad");
+}
