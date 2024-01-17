0c4829c30518711398cf40b4ca41354e41e27caa
DiuMasum <masum35-577@diu.edu.bd>
2024-01-17 11:49:44 +0600

    array

---
+++ array.js
@@ -0,0 +1,14 @@
+// array = a variable like structure that can hold more than 1 value
+
+let fruits = ["apple", "banana", "water melon"];
+
+//fruits[3] = "coconut";
+//fruits.push("coconunt");
+//fruits.pop();
+//fruits.unshift("mango");
+fruits.shift();
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
